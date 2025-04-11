0ceac656bac6083e3ac9f1c37f19ddb374fec288
Egiferdiansyah <egiferdiansyah80@gmail.com>
2025-04-11 10:06:43 +0800

    Add files via upload

---
+++ meet1.py
@@ -0,0 +1,24 @@
+#perbandingan kemungkinan nilai =true/salah(bolean)
+#<, >, >=, <=, != 
+
+a = 24
+b = 76
+hasil = a<b
+print(hasil)
+
+a = 24
+b = 76
+hasil = a>b
+print(hasil)
+
+a = 24
+b = 76
+hasil = a<=b
+print(hasil)
+
+a = 24
+b = 76
+hasil = a != b
+print(hasil)
+
+
